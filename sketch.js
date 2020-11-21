6538f2dfb94ef02087bbfc3ab748f8883a3c3734
chitrakar1991 <70833683+chitrakar1991@users.noreply.github.com>
2020-11-21 13:47:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var land;
+var bin1,bin2,bin3;
+var ball;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	//Create the Bodies Here.
+	land=new Ground(width/2,695,1000,10);
+	ball=new Paper(100,685,20);
+	bin1=new Container(752,655,10,55)
+	bin2=new Container(800,685,110,10)
+	bin3=new Container(850,655,10,55)
+	
+  
+}
+
+
+function draw() {
+  
+  background(0);
+  Engine.update(engine);
+  land.display();
+  ball.display();
+  bin1.display();
+  bin2.display();
+  bin3.display();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(ball.body, ball.body.position,{x:15,y:-20});
+	}
+}
+
+
+
